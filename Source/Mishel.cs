695809700682f364dfb99b3a436e3ed72f9260a9
K.M Hussain <km.hussain@gmx.com>
2018-01-24 14:43:29 +0530

    kmh

--- Source/Mishel.cs
+++ Source/Mishel.cs
@@ -343,19 +343,6 @@ namespace Scripting.Integration
 				return script;			
 			}
 		}
-
-		public virtual object Import(string source)
-		{
-			return ActivateScript(source);
-		}	
-
-		public virtual object Action(string name, params object[] arguments)
-		{
-			if(name.ToLower() == "identify")
-				return this.GetType().Assembly.GetName().Name;
-			return false;	
-		}
-
     	}
 
 	public static class ScriptCaller
@@ -401,49 +388,4 @@ namespace Scripting.Integration
 
 }
 
-namespace Scripting.Interaction
-{
-	public abstract class InteractiveScriptHost : Scripting.Integration.SimpleScriptHost
-	{
-		protected abstract string Input(string prompt, object[] values);
-
-		protected abstract bool Output(string message, bool confirm);
-
-		public virtual void Inform(string text)
-		{
-			Output(text, false);
-		}
-
-		public virtual bool Concur(string confirmation)
-		{
-			return Output(confirmation, true);
-		}
-
-		public virtual object Accept(string prompt, object value=null)
-		{
-			string input = Input(prompt, new object[]{value});
-			if(input == null) return value;
-
-			decimal result;
-			if(decimal.TryParse(input, out result))
-				return result;
-
-			return input;				
-		} 
-
-		public virtual object Choose(string prompt, params object[] choices)
-		{
-			string input = Input(prompt, choices);	
-
-			if(input == null && choices.Length > 0)
-				input = choices[0].ToString();
-
-			decimal result;
-			if(decimal.TryParse(input, out result))
-				return result;
-			
-			return input;		
-		}
-	}
-}
 
